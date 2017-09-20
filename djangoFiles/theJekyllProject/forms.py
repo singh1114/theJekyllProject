07241bb22c9ba6b4823ddcf60e7688b14e89d3b2
singh1114 <ranvir.singh1114@gmail.com>
2017-09-21 03:51:30 +0530

    Add full form of the post

--- djangoFiles/theJekyllProject/forms.py
+++ djangoFiles/theJekyllProject/forms.py
@@ -1,4 +1,28 @@
 from django import forms
+from django.utils import timezone
+from tinymce.models import HTMLField
+
 
 class PostForm(forms.Form):
-    
+    author = forms.CharField(
+        help_text='Name of the author',
+        label='Author',
+        #initial=user.name
+    )
+    comments = forms.BooleanField(
+        help_text='Should the comment be added?',
+        label='Allow commenting'
+    )
+    date = forms.DateField(
+        help_text='Date of posting',
+        initial=timezone.now()
+    )
+    title = forms.CharField(
+        help_text='title of the post',
+    )
+    content = HTMLField()
+
+    # ModelChoiceField or add more categories
+    category = forms.CharField(
+        initial=PostCategory.objects.all()
+    )
