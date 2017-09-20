8cabf771d406e369e990a9a479046eda8db580b6
singh1114 <ranvir.singh1114@gmail.com>
2017-09-21 03:03:04 +0530

    Add models of posts and show it to the admin
    Add swp files to the gitignore
    Add file for the forms

--- djangoFiles/theJekyllProject/models.py
+++ djangoFiles/theJekyllProject/models.py
@@ -3,4 +3,26 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from tinymce.models import HTMLField
+
+class Post(models.Model):
+    author = models.CharField(max_length=100, null=True, blank=True)
+    comments = models.BooleanField()
+    date = models.DateField()
+    layouts = (
+        ('post', 'post'),
+        ('page', 'page')
+    )
+    layout = models.CharField(max_length=100, choices=layouts, null=True, blank=True)
+    title = models.CharField(max_length=2000)
+    content = HTMLField()
+
+    def __str__(self):
+        return self.title + self.date
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post)
+    category = models.CharField(max_length=200, null= True, blank=True)
+
+    def __str__(self):
+        return self.category
