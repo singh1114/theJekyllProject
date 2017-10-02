5d29727535995dbe07730b0397d7f8ee4618da5a
singh1114 <ranvir.singh1114@gmail.com>
2017-10-03 01:14:00 +0530

    Add models of site profile module

--- djangoFiles/theJekyllProject/models.py
+++ djangoFiles/theJekyllProject/models.py
@@ -1,8 +1,8 @@
 #-*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
-
 from ckeditor.fields import RichTextField
 
 class Post(models.Model):
@@ -25,9 +25,143 @@ class Post(models.Model):
     def __str__(self):
         return self.title + str(self.date)
 
+
 class PostCategory(models.Model):
     post = models.ForeignKey(Post)
     category = models.CharField(max_length=200, null= True, blank=True)
 
     def __str__(self):
         return self.category
+
+
+class SiteData(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    name = models.CharField(
+        max_length=200,
+        default='Your site name',
+    )
+    description = models.CharField(
+        max_length=2000,
+        default='Description of the site',
+    )
+    avatar = models.ImageField(
+        upload_to='images/',
+    )
+
+
+class SiteSocialProfile(models.Model):
+    site_data = models.OneToOneField(
+        SiteData,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    dribble = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    email = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    facebook = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    flickr = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    github = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    instagram = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    linkedin = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    pinterest = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    rss = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    twitter = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    stackoverflow = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    youtube = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    googleplus = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    disqus = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    google_analytics = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+
+
+class SitePlugin(models.Model):
+    site_data = models.ForeignKey(
+        SiteData,
+        on_delete=models.CASCADE,
+    )
+    plugin = models.CharField(
+        max_length=200,
+    )
+
+
+class SiteExclude(models.Model):
+    site_data = models.ForeignKey(
+        SiteData,
+        on_delete=models.CASCADE,
+    )
+    exclude = models.CharField(
+        max_length=200,
+    )
+
+
+class SiteTheme(models.Model):
+    site_data = models.OneToOneField(
+        SiteData,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    theme = models.CharField(
+        max_length=200,
+    )
