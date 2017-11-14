e3df459144e0cb8033d3780bbe6fb87b5af28c69
Mona Lisa <abc@gmail.com>
2017-11-14 16:33:49 +0000

    Getting all the code from production environment
    * Used BASE_DIR everywhere in the code
    * Used downloaded static files
    * Used Bootstrap everywhere
    * Fixes #18
    * Fixes #14

--- djangoFiles/theJekyllProject/forms.py
+++ djangoFiles/theJekyllProject/forms.py
@@ -12,9 +12,14 @@ from theJekyllProject.models import SiteData
 
 class RepoForm(forms.Form):
     repo = forms.CharField(
-        label='New repository Name',
+        label='Repo Name',
         max_length=200,
-        help_text='Create a new repository with the following name'
+        help_text='Create a new repository with the following name',
+        widget = forms.TextInput(attrs= {
+                'class': 'form-control',
+                'placeholder': 'blog',
+                'name': 'repo-name'
+            })
     )
 
 
@@ -23,16 +28,34 @@ class AddPostForm(forms.Form):
         label='Author',
         max_length=40,
         help_text='Name of the author',
+        widget = forms.TextInput(attrs= {
+                'class': 'form-control',
+                'placeholder': 'Author...',
+                'name': 'author'
+            })
     )
 
     comments = forms.BooleanField(
         initial=True,
-        required=False
+        required=False,
     )
 
-    date = forms.DateTimeField(
+    date = forms.DateField(
         help_text='Date of posting',
-        initial=timezone.now()
+        initial=timezone.now().date(),
+        widget = forms.TextInput(attrs= {
+                'class': 'form-control date-picker',
+                'name': 'date'
+            })
+    )
+
+    time = forms.TimeField(
+        help_text='Date of posting',
+        initial=timezone.now().time(),
+        widget = forms.TextInput(attrs= {
+                'class': 'form-control',
+                'name': 'date'
+            })
     )
 
     layouts = (
@@ -41,35 +64,64 @@ class AddPostForm(forms.Form):
     )
 
     layout = forms.ChoiceField(
-        choices=layouts
+        choices=layouts,
+        widget = forms.Select(attrs= {
+                'class': 'form-control',
+                'name': 'layout'
+            })
     )
 
     title = forms.CharField(
         help_text='title of the post',
-        max_length=400
+        max_length=400,
+        widget = forms.TextInput(attrs= {
+                'class': 'form-control',
+                'placeholder': 'How to make a website',
+                'name': 'title'
+            })
     )
 
-    content = forms.CharField(widget=CKEditorWidget())
-    # FIXME ModelChoiceField or add more categories
     category = forms.CharField(
-        #initial=PostCategory.objects.all()
-        max_length=20
+        max_length=20,
+        widget=forms.TextInput(attrs= {
+                'class': 'form-control',
+                'placeholder': 'Technology',
+                'name': 'category'
+            }
+        )
     )
 
+    content = forms.CharField(
+        widget=CKEditorWidget(attrs= {
+                'class': 'form-control',
+                'name': 'content'
+            })
+    )
 
 
 class SiteProfileForm(forms.Form):
-    # TODO All these stuff must have some initialized values
     title = forms.CharField(
         max_length=200,
         help_text='Name of the site',
         required=False,
+        widget=forms.TextInput(attrs= {
+                'class': 'form-control',
+                'placeholder': 'JekLog',
+                'name': 'title'
+            }
+        )
     )
     description = forms.CharField(
         max_length=2000,
         help_text='Description of the site',
         initial='Your site description',
         required=False,
+        widget=forms.TextInput(attrs= {
+                'class': 'form-control',
+                'placeholder': 'Web developing guide',
+                'name': 'description'
+            }
+        )
     )
     avatar = forms.ImageField(
         required=False,
@@ -77,16 +129,28 @@ class SiteProfileForm(forms.Form):
 
 
 class SiteSocialProfileForm(forms.Form):
-    # TODO Add initials
     dribbble = forms.CharField(
         max_length=200,
         help_text='Your dribbble username',
-        required=False
+        required=False,
+        widget=forms.TextInput(attrs= {
+                'class': 'form-control',
+                'placeholder': 'Dribbble',
+                'name': 'dribbble'
+            }
+        )
+
     )
     email = forms.EmailField(
         max_length=200,
         required=False,
-        help_text='email id'
+        help_text='email id',
+        widget=forms.EmailInput(attrs= {
+            'class': 'form-control',
+            'placeholder': 'jeklogjek@gmail.com',
+            'name': 'email',
+            }
+        )
     )
     facebook = forms.CharField(
         max_length=200,
@@ -183,11 +247,47 @@ class SiteThemeForm(forms.Form):
 
 
 class ContactForm(forms.Form):
-    first_name = forms.CharField(label="First Name", max_length=200,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ranvir...','name': 'first_name'}))
-    last_name = forms.CharField(label="Last Name", max_length=200,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Singh...','name': 'last_name'}))
-    email = forms.EmailField(label="Email", max_length=200,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'jeklogjek@gmail.com...','name': 'email_name'}))
-    message = forms.CharField(label="Message", max_length=5000,
-                               widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Type your message here..', 'name': 'message'}))
+    first_name = forms.CharField(
+        label="First Name",
+        max_length=200,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ranvir...',
+                'name': 'first_name'
+            }
+        )
+    )
+    last_name = forms.CharField(
+        label="Last Name",
+        max_length=200,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Singh...',
+                'name': 'last_name'
+            }
+        )
+    )
+    email = forms.EmailField(
+        label="Email",
+        max_length=200,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'jeklogjek@gmail.com...',
+                'name': 'email_name'
+            }
+        )
+    )
+    message = forms.CharField(
+        label="Message",
+        max_length=5000,
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Type your message here..',
+                'name': 'message'
+            }
+        )
+    )
