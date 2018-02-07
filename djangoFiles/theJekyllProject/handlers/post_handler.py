1b8b18e948540590376a54f22354e4e82fa74f0c
singh1114 <ranvir.singh1114@gmail.com>
2018-02-08 00:26:58 +0530

    #41 Adds post handler with regex support

--- djangoFiles/theJekyllProject/handlers/post_handler.py
+++ djangoFiles/theJekyllProject/handlers/post_handler.py
@@ -1,3 +1,8 @@
+import os
+
+import re
+
+
 from django.conf import settings
 
 
@@ -11,9 +16,33 @@ class PostHandler:
                                    self.user.username, self.repo_name,
                                    'posts'])
 
+    def handle_post_head(self, head_content):
+        """
+        """
+        # TODO work on this
+        pass
+
+    def handle_post_body(self, body):
+        """
+        """
+        # TODO then this one
+        pass
+
+    def extract_post(self, file):
+        """
+        Extract the post into types of content.
+        """
+        with open(file, 'r') as post_file:
+            file_data = post_file.read()
+
+        regex_search = re.search('---([^-]+)---([^-]+)', file_data)
+        self.handle_post_head(regex_search.group(1))
+        self.handle_post_body(regex_search.group(2))
+
     def read_posts(self):
         """
         Read the _posts directory and iterate over all the files in the
         directory.
         """
-        pass
+        for file in os.listdir(self.posts_path):
+            self.call_that_function(file)
