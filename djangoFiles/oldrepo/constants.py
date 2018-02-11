a800bbb02cda7657e06269079f53c3a9561d1fa9
singh1114 <ranvir.singh1114@gmail.com>
2018-02-12 01:31:01 +0530

    #41 Adds views and starts testing in local

--- djangoFiles/oldrepo/constants.py
+++ djangoFiles/oldrepo/constants.py
@@ -1,3 +1,3 @@
 EARLY_CHECK_FAILS = 'Some early checks has failed.'
-FILES_NOT_FOUND = 'Files we were looking for were not found in the repo'
+FILES_NOT_FOUND = 'Files we were looking for were not found in the repo.'
 OLD_REPO_SUCCESS = 'Parsed successfully.'
--- djangoFiles/oldrepo/urls.py
+++ djangoFiles/oldrepo/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from oldrepo.views import UseOldRepo
 
+app_name = 'oldrepo'
+
 urlpatterns = [
-    url(r'^oldrepo/?', UseOldRepo.as_view(), name='old-repo')
+    url(r'^oldrepo/(?P<repo_name>[\w\-]+)/$', UseOldRepo.as_view(), name='old-repo')
 ]
