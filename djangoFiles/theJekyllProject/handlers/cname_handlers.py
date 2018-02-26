2d78071d0d266ca20569eb925bfcb72172bdf5a1
singh1114 <ranvir.singh1114@gmail.com>
2018-02-27 00:49:36 +0530

    #49 Created some sample methods to handle field data

--- djangoFiles/theJekyllProject/handlers/cname_handlers.py
+++ djangoFiles/theJekyllProject/handlers/cname_handlers.py
@@ -12,5 +12,15 @@ class CNameHandler:
         """
         Load the initials from the database
         """
+        # FIXME What if the cname is empty
         cname = CNameDbIO().get_or_filter({'repo': RepoDbIO().get_repo(user)})
         return form_class(initial=cname.__dict__)
+
+    def assign_cname(self, user):
+        """
+        Assign the Cname to the repo
+        """
+        # FIXME same error will be encountered
+        cname = CNameDbIO().get_or_filter({'repo': RepoDbIO().get_repo(user)})
+        if cname is '':
+            CNameDbIO().create_or_update({})
