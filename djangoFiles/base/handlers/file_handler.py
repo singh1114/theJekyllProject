766b752ac74074e6849839e2c7d88b207d51f2b2
singh1114 <ranvir.singh1114@gmail.com>
2018-02-28 00:59:45 +0530

    #49 creates the full flow for adding the cnames to the repo

---
+++ djangoFiles/base/handlers/file_handler.py
@@ -0,0 +1,21 @@
+class FileHandler:
+    """
+    All the operations related to files can be handled here
+    """
+    def __init__(self, file_path, file_name):
+        self.file_path = file_path
+        self.file_name = file_name
+
+    def read_file(self):
+        """
+        Read the content of the file
+        """
+        with open('/'.join([self.file_path, self.file_name]), 'r') as file:
+            return file.read()
+
+    def write_full_file(self, content):
+        """
+        Write file changing the whole content
+        """
+        with open('/'.join([self.file_path, self.file_name]), 'w') as file:
+            return file.write(content)
