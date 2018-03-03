9f09d670d463332d02164d7f7591ec1d6fc6d30d
singh1114 <ranvir.singh1114@gmail.com>
2018-03-04 03:54:50 +0530

    environment variables for db data

--- djangoFiles/djangoFiles/settings.py
+++ djangoFiles/djangoFiles/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'djangoFiles.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'jeklog',
-        'USER': 'jeklog',
-        'PASSWORD': 'jeklog',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
         'HOST': 'localhost',
         'PORT': '5432',
     }
