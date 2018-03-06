e74a0ce76eb03cfa419bd2ef5f920bee35d71ed7
singh1114 <ranvir.singh1114@gmail.com>
2018-03-06 23:57:57 +0530

    #49 Corrects form handlers functions

--- djangoFiles/base/handlers/form_handler.py
+++ djangoFiles/base/handlers/form_handler.py
@@ -2,7 +2,7 @@ class FormHandler:
     """
     We would use this class for handling all the form events
     """
-    def __init__(self, request, form_class, form_fields):
+    def __init__(self, request, form_class):
         self.request = request
         self.form_class = form_class
 
@@ -10,6 +10,7 @@ class FormHandler:
         """
         This function is used to post the form fields
         """
+        # FIXME something wrong with these functions
         field_dict = {}
         if(self.form_class(self.request.POST).is_valid()):
             for field in fields:
