80a4b15848b8935d5816ff6e3c9258bd19c0c9ed
singh1114 <ranvir.singh1114@gmail.com>
2018-03-10 04:14:33 +0530

    #49 Adds the logic to push code to remote

---
+++ djangoFiles/base/handlers/bash_script.py
@@ -0,0 +1,16 @@
+import subprocess
+
+from django.conf import settings
+
+
+class BashScript:
+    def __init__(self):
+        pass
+
+    def push_online(self, user, repo):
+        base_dir = settings.BASE_DIR
+        path = '/'.join([base_dir, '..', 'gitsendupstream.sh'])
+        task = subprocess.Popen(['/bin/bash', path, user.username, repo.repo,
+                                base_dir])
+        if task.returncode !=0:
+            raise BaseException({'message': 'push error'})
