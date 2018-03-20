a0c40fe9d8ca4213129cc756a97e4e1d51ee4f22
singh1114 <ranvir.singh1114@gmail.com>
2018-03-21 00:16:22 +0530

    Adds handlers to main file and run them

--- djangoFiles/startbootstrap/handlers.py
+++ djangoFiles/startbootstrap/handlers.py
@@ -1,4 +1,5 @@
 from base.handlers.github_handler import GithubHandler
+from base.handlers.path_handlers import PathHandler
 
 from startbootstrap.constants import StartBootstrap
 
@@ -8,10 +9,22 @@ class StartBootstrapHandler:
         self.repo = repo
         self.user = user
         self.gh_handler = GithubHandler(user, repo)
+        self.path = create_repo_path(user, repo)
 
-    def create_repo(self):
-        auth_token = self.gh_handler.get_user_token()
-        return self.gh_handler.create_remote_repo(auth_token)
-
-    def clone_theme_repo(self, path):
-        return self.gh_handler.clone_repo(StartBootstrap.FORKED_URL, path)
+    def perform_initial_tasks(self):
+        """
+        Perform initial tasks. Tasks include:
+        * get user token
+        * create repo
+        * clone repo to the particular location
+        * change config of the git repo
+        * commit all changes
+        * push code to remote
+        """
+        user_token = self.gh_handler.get_user_token()
+        self.gh_handler.create_remote_repo(user_token)
+        repo_path = PathHandler(self.user, self.repo).create_repo_path()
+        repo = self.gh_handler.clone_repo(StartBootstrap.FORKED_URL, repo_path)
+        self.gh_handler.change_config()
+        self.gh_handler.commit_all_changes(repo, 'Intial commit')
+        self.push_code(repo)
