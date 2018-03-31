93bcc1aa217366ac833db3e770ae4f2de029295d
singh1114 <ranvir.singh1114@gmail.com>
2018-04-01 02:13:58 +0530

    Adds social data forms, views and urls

--- djangoFiles/startbootstrap/views.py
+++ djangoFiles/startbootstrap/views.py
@@ -12,9 +12,11 @@ from django.views.generic.edit import FormView
 from base.handlers.form_handler import FormHandler
 
 from startbootstrap.constants import TemplateName
-from startbootstrap.forms import SiteProfileForm
+from startbootstrap.forms import SiteProfileForm, SiteSocialForm
 from startbootstrap.handlers.sbs_handlers import SBSHandler
 from startbootstrap.handlers.sbs_form_handlers import SBSFormHandler
+
+from theJekyllProject.dbio import RepoDbIO
 
 
 class StartBootstrapThemeView(LoginRequiredMixin, View):
@@ -33,20 +35,22 @@ class StartBootstrapThemeView(LoginRequiredMixin, View):
         return HttpResponseRedirect(reverse('site-data'))
 
 
-class SBSDataView(LoginRequiredMixin, FormView):
+class SBSSiteDataView(LoginRequiredMixin, FormView):
     """
     Form View to handle site data of the startbootstrap theme
     """
     form_class = SiteProfileForm
 
     def get(self, request, *args, **kwargs):
-        response = SBSFormHandler.load_site_initials(
-            request.user, self.form_class)
+        repo_name = RepoDbIO().get_repo(request.user).repo
+        form_response = SBSFormHandler(
+            request.user, repo_name).load_site_initials(
+            request, self.form_class)
 
-        return render(request, TemplateName.SBS_SITE_DATA, {'form': response})
+        return render(request,
+                      TemplateName.SBS_SITE_DATA, {'form': form_response})
 
     def post(self, request, *args, **kwargs):
-        # TODO create the _config.yml file commit and push the changes
         form_field_dict = FormHandler(
             request, self.form_class).handle_post_fields((
                 'title',
@@ -55,7 +59,39 @@ class SBSDataView(LoginRequiredMixin, FormView):
                 'baseurl')
         )
         user = request.user
-        SBSFormHandler().post_site_data(user, form_field_dict)
+        repo_name = RepoDbIO().get_repo(request.user).repo
+        SBSFormHandler(user,
+                       repo_name).post_site_data(user, form_field_dict)
 
         return render(request, TemplateName.SBS_SITE_DATA,
                       {'msg': 'Site data updated successfully.'})
+
+
+class SBSSocialDataView(LoginRequiredMixin, FormView):
+    form_class = SiteSocialForm
+
+    def get(self, request, *args, **kwargs):
+        repo_name = RepoDbIO().get_repo(request.user).repo
+        form_response = SBSFormHandler(
+            request.user, repo_name).load_social_profile_initials(
+            request, self.form_class)
+
+        return render(request,
+                      TemplateName.SBS_SOCIAL_DATA,
+                      {'form': form_response})
+
+    def post(self, request, *args, **kwargs):
+        form_field_dict = FormHandler(
+            request, self.form_class).handle_post_fields((
+                'email',
+                'facebook',
+                'github',
+                'twitter')
+        )
+        user = request.user
+        repo_name = RepoDbIO().get_repo(request.user).repo
+        SBSFormHandler(
+            user, repo_name).post_social_profile_data(user, form_field_dict)
+
+        return render(request, TemplateName.SBS_SOCIAL_DATA,
+                      {'msg': 'Social data updated successfully.'})
